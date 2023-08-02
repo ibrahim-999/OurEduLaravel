65adae8f285c79ed0c9b29ddb98232a131ff3d36
“ibrahim-999” <ibrahimkhalaf99@gmail.com>
2023-08-03 01:23:21 +0300

    [Add] UserTransactionController api

---
+++ laravel/app/Http/Controllers/Api/v1/UserTransactionController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Services\TransactionService;
+use Illuminate\Http\Request;
+
+class UserTransactionController extends Controller
+{
+    private TransactionService $transactionService;
+
+    public function __construct(TransactionService $transactionService)
+    {
+        $this->transactionService = $transactionService;
+    }
+
+    public function userTransactions(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $data = $this->transactionService->readProviders();
+        if ($request->query('statusCode')) {
+            $data =  $this->transactionService->statusCode($request->query('statusCode'), $data);
+        }
+        if ($request->query('currency')) {
+
+            $data = $this->transactionService->currency($request->query('currency'), $data);
+        }
+        if ($request->query('amounteMin') && $request->query('amounteMax')) {
+            $data =  $this->transactionService->amountRange($request, $data);
+        }
+        if ($request->query('startDate') && $request->query('endDate')) {
+            $data =  $this->transactionService->dateRange($request, $data);
+        }
+        if ($data) {
+            return response()->json($data);
+        } else {
+            return response()->json('No data is available for this search');
+        }
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\UserTransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('v1')
+    ->group(function () {
+        Route::controller(UserTransactionController::class)
+            ->group(function () {
+                Route::get('transactions', 'userTransactions');
+            });
+    });
